28e0f5040f82b16650490e3c9cea4952719c0acf
Lucas Lemes <llemes-f@student.42sp.org.br>
2021-02-14 14:01:09 -0300

    Create file libft.h

--- libft.h
+++ libft.h
@@ -1,34 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: llemes-f <llemes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/12/07 16:15:07 by pbillett          #+#    #+#             */
-/*   Updated: 2021/02/14 14:00:43 by llemes-f         ###   ########.fr       */
+/*   Created: 2021/02/14 13:55:31 by llemes-f          #+#    #+#             */
+/*   Updated: 2021/02/14 13:57:51 by llemes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft.h"
-#include <stdlib.h>
+#ifndef LIBFT_H
+#define LIBFT_H
 
 char		ft_substr(char const *s, unsigned int start, size_t len)
-{
-	size_t	i;
-	char	*substr;
-
-	i = 0;
-	if (!(s))
-		return (NULL);
-	substr = (char *)malloc((len + 1) * sizeof(*s));
-	if (substr == NULL)
-		return (NULL);
-	while (i < len)
-	{
-		substr[i] = s[start + i];
-		i++;
-	}
-	substr[i] = '\0';
-	return (substr);
-}
