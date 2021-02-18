60a47589833fa8b6be52f4e31d80c96617b3aafe
Lucas Lemes <llemes-f@student.42sp.org.br>
2021-02-18 20:43:40 -0300

    Adjustments to meet the standard

--- src/ft_memmove.c
+++ src/ft_memmove.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llemes-f <llemes-f@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/18 20:40:21 by llemes-f          #+#    #+#             */
+/*   Updated: 2021/02/18 20:41:14 by llemes-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../libft.h"
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	int		i;
 	char	*dest_aux;
@@ -9,19 +21,17 @@ void *ft_memmove(void *dest, const void *src, size_t n)
 	i = 0;
 	dest_aux = (char*)dest;
 	src_aux = (char*)src;
-	
 	if (dest_aux < src_aux)
-		while (len >= 0)
+		while (n >= 0)
 		{
-			dest_aux[len] = src_aux[len];
-			lem--;
+			dest_aux[n] = src_aux[n];
+			n--;
 		}
 	else
-		while (i < len)
+		while (i < n)
 		{
 			dest_aux[i] = src_aux[i];
 			i++;
 		}
-
 	return (dest);
 }
