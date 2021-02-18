21c535ccaedfc7fb7fd0b1be51382eda8ae1f0b1
repl.it user <replituser@example.com>
2021-02-18 16:17:40 +0000

    Maintence files

--- src/ft_memccpy.c
+++ src/ft_memccpy.c
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-void *memccpy(void *dest, const void *src, int c, size_t n)
-{
-  int i;
-  void *dest_aux;
-
-  i = 0;
-  dest_aux = dest;
-  while (i < size_t)
-  {
-    dest[i] = src[i];
-    if ((char*)src[i] == c)
-      return (dest_aux);
-    i++;
-  }
-  return (dest_aux);
-}
-=======
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -46,4 +28,3 @@ void	*memccpy(void *dest, const void *src, int c, size_t n)
 	}
 	return (dest_aux);
 }
->>>>>>> bdc1209714b8605d240880982ac6f370b871d272
