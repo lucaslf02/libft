eb78b2876a4eb2140dddeed78c2a3e3cc6a58662
Lucas Lemes <llemes-f@student.42sp.org.br>
2021-02-26 20:59:03 -0300

    Function ft_itoa implementation finished

--- src/ft_itoa.c
+++ src/ft_itoa.c
@@ -6,13 +6,12 @@
 /*   By: llemes-f <llemes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/25 21:09:14 by llemes-f          #+#    #+#             */
-/*   Updated: 2021/02/25 22:16:05 by llemes-f         ###   ########.fr       */
+/*   Updated: 2021/02/26 20:58:27 by llemes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 #include "stdlib.h"
-#include <stdio.h>
 
 char	*ft_itoa(int n)
 {
@@ -22,33 +21,23 @@ char	*ft_itoa(int n)
 	unsigned int	aux;
 
 	aux = n < 0 ? n * (-1) : n;
-	len = 0;
+	len = n < 0 ? (1) : 0;
 	while ((aux /= 10) > 0)
-	{
 		len++;
-	}
-		
-	len = n < 0 ? (len + 1) : len;
 	aux = n < 0 ? n * (-1) : n;
-	if(!(nbr = (char *)malloc(sizeof(char) * len)))
+	if (!(nbr = (char *)malloc(sizeof(char) * len)))
 		return (NULL);
 	nbr_ptr = nbr;
-	if(n < 0)
+	if (n < 0)
 	{
 		*nbr++ = '-';
 		len--;
 	}
-	printf("len: %i\n", len);
 	while (len != -1)
 	{
-		printf("aux: %i \n", aux);
-		printf("len: %i\n", len);
-		printf("ft_pow: %i\n", ft_pow(10, len));
-		printf("char: %d\n\n", ((aux / ft_pow(10, len)) + 48));
 		*nbr++ = ((aux / ft_pow(10, len)) + 48);
-		aux %= ft_pow(10, len--);		
+		aux %= ft_pow(10, len--);
 	}
 	*nbr = '\0';
 	return (nbr_ptr);
 }
-
