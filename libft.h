6a659aba09fc71a66401ddfa75e46a9efdc384e2
Lucas Lemes <llemes-f@student.42sp.org.br>
2021-02-27 11:20:34 -0300

    Function fd_putendl_fd implementation

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: llemes-f <llemes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 13:55:31 by llemes-f          #+#    #+#             */
-/*   Updated: 2021/02/27 10:53:11 by llemes-f         ###   ########.fr       */
+/*   Updated: 2021/02/27 11:02:22 by llemes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,5 +51,6 @@ int		ft_pow(int base, int exp);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 
 #endif
--- test/ft_putendl_fd_main.c
+++ test/ft_putendl_fd_main.c
@@ -1,19 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*   ft_putendl_fd_main.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: llemes-f <llemes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/27 11:01:20 by llemes-f          #+#    #+#             */
-/*   Updated: 2021/02/27 11:19:10 by llemes-f         ###   ########.fr       */
+/*   Created: 2021/02/14 11:55:03 by llemes-f          #+#    #+#             */
+/*   Updated: 2021/02/27 11:08:37 by llemes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
+#include "stdio.h"
 
-void	ft_putendl_fd(char *s, int fd)
+int main(void)
 {
-	ft_putstr_fd(s, fd);
-	ft_putchar_fd('\n', fd);
+    FILE *inp;
+    inp = fopen("/home/lucas/Documentos/entrada.txt","w");
+    ft_putendl_fd("Lucas Lemes Ferreira", fileno(inp));
+    fclose(inp);
 }
