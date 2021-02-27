c5cb7ec47d49fddbd4468a673e360c5bbc57b915
Lucas Lemes <llemes-f@student.42sp.org.br>
2021-02-27 18:43:40 -0300

    Function ft_lstadd_back implementation

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: llemes-f <llemes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 13:55:31 by llemes-f          #+#    #+#             */
-/*   Updated: 2021/02/27 18:28:03 by llemes-f         ###   ########.fr       */
+/*   Updated: 2021/02/27 18:37:35 by llemes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,5 +63,6 @@ t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int	ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
