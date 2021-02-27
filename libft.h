0116f52d270332a0f4b575bdc28ea73b4889b67a
Lucas Lemes <llemes-f@student.42sp.org.br>
2021-02-27 18:52:11 -0300

    Function ft_lstdeloner implementation

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: llemes-f <llemes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 13:55:31 by llemes-f          #+#    #+#             */
-/*   Updated: 2021/02/27 18:37:35 by llemes-f         ###   ########.fr       */
+/*   Updated: 2021/02/27 18:45:11 by llemes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int	ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
 
 #endif
