e92168b156e0107692afdb16a0b78282ec7e715d
Lucas Lemes <llemes-f@student.42sp.org.br>
2021-02-27 18:35:00 -0300

    Function ft_lstlast implementation

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: llemes-f <llemes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 13:55:31 by llemes-f          #+#    #+#             */
-/*   Updated: 2021/02/27 18:18:03 by llemes-f         ###   ########.fr       */
+/*   Updated: 2021/02/27 18:28:03 by llemes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,6 @@ void    ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int	ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
