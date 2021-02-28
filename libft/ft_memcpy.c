a3608bd8556aa7e7e499dda86c0b0d34f1fb1080
Lucas Lemes <llemes-f@student.42sp.org.br>
2021-02-28 18:57:56 -0300

    Ajustes

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: llemes-f <llemes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/17 20:57:11 by llemes-f          #+#    #+#             */
-/*   Updated: 2021/02/28 18:35:00 by llemes-f         ###   ########.fr       */
+/*   Updated: 2021/02/28 18:57:26 by llemes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t	i;
 	void	*dest_aux;
 
-	if (!dest || !src)
+	if (!dest && !src)
 		return (NULL);
 	i = 0;
 	dest_aux = dest;
