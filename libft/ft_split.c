59fca160a1c8db9ce563eb638af095d1d240c524
Lucas Lemes <llemes-f@student.42sp.org.br>
2021-03-01 21:28:40 -0300

    Ajuste

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,30 +6,30 @@
 /*   By: llemes-f <llemes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 20:58:02 by llemes-f          #+#    #+#             */
-/*   Updated: 2021/03/01 21:26:02 by llemes-f         ###   ########.fr       */
+/*   Updated: 2021/03/01 21:28:12 by llemes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int			number_str(char const *s, char c)
+static int			count_words(char const *s, char c)
 {
-	int	i;
-	int	n_str;
+	size_t word_count;
+	size_t skipper;
 
-	i = 0;
-	n_str = 0;
-	if (s[i] == '\0')
-		return (0);
-	while (s[i + 1])
+	word_count = 0;
+	skipper = 1;
+	while (*s)
 	{
-		if (s[i] == c && s[i + 1] != c)
-			n_str++;
-		i++;
+		if (*s != c && skipper)
+		{
+			skipper = 0;
+			word_count++;
+		}
+		else if (*s++ == c)
+			skipper = 1;
 	}
-	if (s[i] != c)
-		n_str++;
-	return (n_str);
+	return (word_count);
 }
 
 static char const	*skip_equal_chars(char const *s, char c)
@@ -65,10 +65,11 @@ char				**ft_split(char const *s, char c)
 	size_t	word_count;
 	char	**words;
 
-	if (!s)
+	if (s == NULL)
 		return (NULL);
-	word_count = number_str(s, c);
-	if (!(words = (char **)malloc(sizeof(char*) * (word_count + 1))))
+	word_count = count_words(s, c);
+	words = malloc(sizeof(char **) * (word_count + 1));
+	if (words == NULL)
 		return (NULL);
 	create_words(words, s, c, word_count);
 	return (words);
