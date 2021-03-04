d25e96f4767f6e7f88a4e1adda9b4cd93f6ca6d7
Lucas Lemes <llemes-f@student.42sp.org.br>
2021-03-03 22:32:01 -0300

    Ajustes

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,34 +6,28 @@
 /*   By: llemes-f <llemes-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 10:34:00 by llemes-f          #+#    #+#             */
-/*   Updated: 2021/03/03 22:30:32 by llemes-f         ###   ########.fr       */
+/*   Updated: 2021/03/03 22:31:53 by llemes-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(char *str)
+int	ft_atoi(const char *nptr)
 {
-	int	sum;
-	int	sign;
-	int	found;
+	int	nbr;
+	int	signal;
 
-	sum = 0;
-	sign = 1;
-	found = 1;
-	while (*str == ' ' || *str == '\t' || *str == '\n' || *str == '\f' || *str == '\r')
-		str++;
-	if (*str == '-')
-		sign = -1;
-	if (*str == '-' || *str == '+')
-		str++;
-	while (*str && found)
+	nbr = 0;
+	signal = 1;
+	while (ft_isspace(*nptr))
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+		if (*nptr++ == '-')
+			signal = -1;
+	while (ft_isdigit(*nptr))
 	{
-		if (*str >= '0' && *str <= '9')
-			sum = sum * 10 + *str - '0';
-		else
-			found = 0;
-		str++;
+		nbr = nbr * 10 + (*nptr - 48);
+		nptr++;
 	}
-	return (sign * sum);
+	return (nbr * signal);
 }
